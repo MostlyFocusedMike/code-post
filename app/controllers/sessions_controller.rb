9ce33d6f83c9c179e9331cd041f41ea62a131b60
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 11:22:41 -0400

    add basic login functionality that bounces back users not in DB or incorrect passwords

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end 
 
   def create 
-
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else 
+      render "new"
+    end 
   end 
 
   def destroy
