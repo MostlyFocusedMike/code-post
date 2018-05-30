7bf4fe5d8e21e2844c2519b762cce61fc091782c
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 10:33:01 -0400

    add basic name and email validations, leave password validations for later so testing is easier

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,5 +4,8 @@ class Profile < ApplicationRecord
   has_many :comments
   has_many :commented_posts, :through => :comments, :source => :post
   has_secure_password
+  validates :name, :email, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
   validates_confirmation_of :password
 end
