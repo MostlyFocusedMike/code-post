9423f3f6af6693cdddf21fda0306b745d0ad094f
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 11:53:15 -0400

    add current_user and matched_user methods in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end 
+
+  def matched_user?(user)
+    current_user.id == user.id
+  end 
+  # now you can call this method in your views by adding this line
+  helper_method :current_user
+
 end
