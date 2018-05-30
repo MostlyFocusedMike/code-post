633e0b40b243614bf4b57d340971797de48df0f9
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 17:55:53 -0400

    add create functionality to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,12 +18,22 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "new"
+    end
   end
 
   private
 
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
     end
 end
