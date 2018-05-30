7b9cb81bdcb4e5e0dcb5687c1002eafc0051d0e6
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 16:18:36 -0400

    Add basic index and show pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
-
+    @posts = Post.all
   end
 
   def show
@@ -8,10 +9,16 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
 
   end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
