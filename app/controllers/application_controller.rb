1641153cafd3236761a018f1f56c0988e9ea9f7a
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-30 12:03:50 -0400

    add logged in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end 
 
+  def logged_in?
+    # this is extra explicit
+    session[:user_id] ? true : false 
+  end 
+
   def matched_user?(user)
     current_user.id == user.id if current_user
   end 
   # now you can call this method in your views by adding this line
-  helper_method :matched_user?
+  helper_method :matched_user?, :logged_in?
 
 end
