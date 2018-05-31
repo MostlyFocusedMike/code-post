26dfb05172543358e998a07ebeca81aa73dbfdb4
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 15:53:47 -0400

    add migration for url column in posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_192859) do
+ActiveRecord::Schema.define(version: 2018_05_31_194315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_05_31_192859) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
