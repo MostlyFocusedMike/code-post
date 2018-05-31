90f35354d2987a79e41a6bfd1b96e3fbc8bcb37e
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 12:13:20 -0400

    add login form to posts page, with rough styling for readability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
-  def new 
+  def new
 
-  end 
+  end
 
-  def create 
+  def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       redirect_to posts_path
-    else 
+    else
       render "new"
-    end 
-  end 
+    end
+  end
 
   def destroy
     session.delete :user_id
-    redirect_to login_path
-  end 
-end 
+    redirect_to posts_path
+  end
+end
