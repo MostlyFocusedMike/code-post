2eab61a378accb656b02e93617ddbad192dd7623
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 14:39:32 -0400

    use a where search for login sessions instead of find_by, so we can search case insensitively

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.where('LOWER(username) = LOWER(?)', params[:username])[0]
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to posts_path
