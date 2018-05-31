768cde69d64a205ece3ed32ab3d0fac2357a45de
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 14:01:01 -0400

    partial show form for user page and user comment page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,29 @@
 class CommentsController < ApplicationController
-  def create 
-    # the request.referer returns the page the form came from: 
+  def index
+    
+    @user = User.find(params[:user_id])
+  end
+  def create
+    # the request.referer returns the page the form came from:
     #     https:/site.com/posts/2
-    # so we use regex to pull the post number to use as our id 
+    # so we use regex to pull the post number to use as our id
    content = params[:comment][:content]
-   
-   # checks to see if there are any words in the comment 
+
+   # checks to see if there are any words in the comment
    if content =~ /\w/
      user_id = session[:user_id]
      Comment.create(user_id: user_id, post_id: params[:post_id], content: content)
-   else 
+   else
      flash[:comment_warning] = "Don't leave a blank comment"
-   end 
+   end
    redirect_to post_path(params[:post_id])
-  end 
+  end
 
   def destroy
     comment = Comment.find(params[:id])
     post = comment.post
-    comment.destroy 
+    comment.destroy
     redirect_to post_path(post)
-  end 
+  end
 
-end 
+end
