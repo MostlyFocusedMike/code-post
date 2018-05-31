546d97552413a79add64c3612af1d63269116617
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 14:20:49 -0400

    clean up views for the two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index', as: "posts"
-  resources 'posts', only: [:show, :new, :create] do # remove user index for actual production
+  post '/', to: 'posts#create'
+  resources 'posts', only: [:show, :new] do # remove user index for actual production
     resources 'comments', only: [:create, :destroy]
   end
   resources 'users', except: [:index, :destroy] do # remove user index for actual production
