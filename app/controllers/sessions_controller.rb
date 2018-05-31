8265e10078610e52707b651379aa9e7fb7265ba3
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-05-31 14:25:45 -0400

    Add incorrect password/username warning to login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to posts_path
     else
+      flash[:log_in_warning] = "Your password or username was incorrect."
       render "new"
     end
   end
