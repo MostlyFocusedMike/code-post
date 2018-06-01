bbd4b16eba3f1d498bf1decce971c85e2f3ddd17
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-06-01 03:48:43 -0400

    add styling to create new posts and post show page, added missing validations to create post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+  validates :title, :content, presence: true
   attr_accessor :commentor_count
 
   def has_link?
