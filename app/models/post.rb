a3950eab50ecc92c9d2e0227f62d30fe56eec779
cjunks94 <cjunks94@gmail.com>
2018-05-31 21:47:34 -0400

    im hella dumb and made things way harder

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+  attr_accessor :commentor_count
 
   def has_link?
     !!self.url && self.url.length > 0
@@ -11,15 +12,7 @@ class Post < ApplicationRecord
     self.comments.length
   end
 
-  def unique_user_count #not very nice at all
-    arr=[]
-    count=0
-    self.comments.each do |comment|
-      if !arr.include?(comment.user)
-        arr << comment.user
-        count +=1
-      end
-    end
-    return count
+  def unique_user_count #im dumb
+    self.users.uniq.length
   end
 end
