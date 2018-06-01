d1fc2c3ce4dbb5c815ff11e848ba753c344c8754
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-06-01 11:15:29 -0400

    change new_user to signup route and fix all views that used that path

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index', as: "posts"
   post '/', to: 'posts#create'
+  get '/signup', to: 'users#new'
   resources 'posts', only: [:show, :new, :destroy] do # remove user index for actual production
     resources 'comments', only: [:create, :destroy]
   end
-  resources 'users', except: [:index, :destroy] do # remove user index for actual production
+  resources 'users', except: [:new, :index, :destroy] do # remove user index for actual production
     resources 'comments', only: [:index]
   end
   get 'users/:id/edit_password', to: "users#edit_password", as: "edit_password"
